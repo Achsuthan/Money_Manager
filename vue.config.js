94f95915b31b3468c66b689a404cca2cb4ce151b
Achsuthan <achsuthan@icloud.com>
2021-09-18 10:42:55 +0800

    repo added to config

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,8 @@ module.exports = {
   devServer: {
     disableHostCheck: true,
   },
-
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/money_manager_frontend/'
+  : '/',
   transpileDependencies: ['vuetify'],
 }
