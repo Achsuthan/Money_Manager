5f3c8f6fd0d17dabf0a1fd64147511baefa15b56
Achsuthan <achsuthan@icloud.com>
2021-10-29 00:20:11 +0800

    Profile code aligned

--- src/views/dashboard/pages/Profile/Profile.vue
+++ src/views/dashboard/pages/Profile/Profile.vue
@@ -13,24 +13,26 @@
             </v-list-item>
           </template>
           <v-card elevation="2">
-    <v-card-text class="grow pa-0">
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-subtitle class="text-h4 font-weight-regular text-wrap">
-            <br/>
-            Name : <span style="color: black;"> {{name}} </span>
-          </v-list-item-subtitle>
-           <v-list-item-subtitle class="text-h4 font-weight-regular text-wrap">
-            <br/>
-            Email : <span style="color: black;"> {{email}} </span>
-          </v-list-item-subtitle>
-        </v-list-item-content>
-        <v-btn color="error" @click="logout()">
-            Logout
-        </v-btn>
-      </v-list-item>
-    </v-card-text>
-  </v-card>
+            <v-card-text class="grow pa-0">
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-subtitle
+                    class="text-h4 font-weight-regular text-wrap"
+                  >
+                    <br />
+                    Name : <span style="color: black"> {{ name }} </span>
+                  </v-list-item-subtitle>
+                  <v-list-item-subtitle
+                    class="text-h4 font-weight-regular text-wrap"
+                  >
+                    <br />
+                    Email : <span style="color: black"> {{ email }} </span>
+                  </v-list-item-subtitle>
+                </v-list-item-content>
+                <v-btn color="error" @click="logout()"> Logout </v-btn>
+              </v-list-item>
+            </v-card-text>
+          </v-card>
         </base-material-card>
       </v-col>
     </v-row>
@@ -40,19 +42,19 @@
 <script>
 export default {
   data: () => ({
-      name: String,
-      email: String,
+    name: String,
+    email: String,
   }),
   created() {
-      const user = JSON.parse(localStorage.getItem("user"))
-      this.name = user.name;
-      this.email = user.email;
+    const user = JSON.parse(localStorage.getItem("user"));
+    this.name = user.name;
+    this.email = user.email;
   },
-  methods:{
-      logout(){
-          localStorage.removeItem("user");
-          location.reload();
-      }
-  }
+  methods: {
+    logout() {
+      localStorage.removeItem("user");
+      location.reload();
+    },
+  },
 };
 </script>
